c3b65efcd1aa720f57b8fb4d79e1dac1c44bc979
thewa1818 <zawa.i.0218@gmail.com>
2023-06-06 14:15:08 +0900

    変更

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+
+$("h1").on("click", function () {
+  $(".omikuji_kekka").html("あああ");
+
+  const random = Math.floor(Math.random() * 5);
+
+  if (random === 0){
+    console.log("大吉です");
+    $(".omikuji_kekka").html("大吉です");
+  }else if (random === 1){
+    console.log("中吉です");
+    $(".omikuji_kekka").html("中吉です");
+  }else if (random === 2){
+    console.log("吉です");
+    $(".omikuji_kekka").html("吉です");
+  }else if (random === 3){
+    console.log("小吉です");
+    $(".omikuji_kekka").html("小吉です");
+  }else if (random === 4){
+    console.log("末です");
+    $(".omikuji_kekka").html("末です");
+  }
+});
