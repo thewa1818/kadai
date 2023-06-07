af6bce42261fa140864d9b7d2a8cb8aa5f8b4827
thewa1818 <zawa.i.0218@gmail.com>
2023-06-07 22:46:50 +0900

    img

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 
 
 $("h1").on("click", function () {
-  $(".omikuji_kekka").html("あああ");
+  $(".omikuji_kekka").fadeIn();
 
   const random = Math.floor(Math.random() * 5);
 
   if (random === 0){
     console.log("大吉です");
-    $(".omikuji_kekka").html("大吉です");
-    $(".omikuji_kekka").html("<img src='img/daikichi.webp'>");
+    $(".omikuji_kekka").html("<img src='img/daikichi.png'>");
   }else if (random === 1){
     console.log("中吉です");
-    $(".omikuji_kekka").html("中吉です");
+    $(".omikuji_kekka").html("<img src='img/chukichi.png'>");
   }else if (random === 2){
     console.log("吉です");
-    $(".omikuji_kekka").html("吉です");
+    $(".omikuji_kekka").html("<img src='img/kichi.png'>");
   }else if (random === 3){
     console.log("小吉です");
-    $(".omikuji_kekka").html("小吉です");
+    $(".omikuji_kekka").html("<img src='img/chokichi.png'>");
   }else if (random === 4){
     console.log("末です");
-    $(".omikuji_kekka").html("末です");
+    $(".omikuji_kekka").html("<img src='img/suekichi.png'>");
   }
 });
