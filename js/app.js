7ad5209415fe9d4dc68d6f9f7fabd1a7dd8a40df
thewa1818 <zawa.i.0218@gmail.com>
2023-06-11 10:20:23 +0900

    fade

--- js/app.js
+++ js/app.js
@@ -101,3 +101,17 @@ $(".facebook_img").on("mouseout",function(){
   $(this).removeClass("fb");
   $(".facebook_text").css("display","none");
 })
+
+$(function () {
+  $(window).scroll(function () {
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $(".qustion").each(function () {
+      const targetPosition = $(this).offset().top;
+      if (scroll > targetPosition - windowHeight + 100) {
+        $(this).addClass("is-fadein");
+      }
+    });
+  });
+});
